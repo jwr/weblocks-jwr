103af3eb7051f0c6263394d25caf4f0ff493556c
Jan Rychter <jan@rychter.com>
2008-12-21 13:28:18 +0100

    separate navigation content rendering, add teleport widget

--- src/widgets/navigation.lisp
+++ src/widgets/navigation.lisp
@@ -1,8 +1,8 @@
 
 (in-package :weblocks)
 
-(export '(navigation render-navigation-menu init-navigation make-navigation))
-
+(export '(navigation render-navigation-menu init-navigation make-navigation
+	  navigation-render-content))
 
 (defwidget navigation (static-selector)
   ((pane-names :accessor navigation-pane-names
@@ -10,7 +10,15 @@
 	       :initform nil
 	       :documentation "An alist mapping url-tokens to
 	       human-readable pane names (rendered as a menu). Use nil
-	       as the key for the default item."))
+	       as the key for the default item.")
+   (header :accessor navigation-header
+	   :initarg :header
+	   :initform nil
+	   :documentation "A heading that will be rendered in a <h1> tag")
+   (render-content :accessor navigation-render-content
+		   :initarg :render-content
+		   :initform t
+		   :documentation "Whether navigation should also render its contents"))
   (:documentation "The navigation widget can act as a menu controls, a
   tabbed control, etc. It is a convenience combination of the
   static-selector widget and a menu snippet."))
@@ -29,17 +37,16 @@ may be NIL in which case the default pane name is provided."
 				 (compose-uri-tokens-to-url (car pane))))
 			 (static-selector-panes obj))
 		 :selected-pane (static-selector-current-pane obj)
-		 :header (if (widget-name obj)
-			     (humanize-name (widget-name obj))
-			     "Navigation")
+		 :header (navigation-header obj)
 		 :container-id (ensure-dom-id obj)
 		 :empty-message "No navigation entries")))
 
 (defmethod render-widget-body ((obj navigation) &rest args)
   (apply #'render-navigation-menu obj args)
-  (with-html 
-    (:div :class "navigation-body"
-	  (mapc #'render-widget (widget-children obj)))))
+  (when (navigation-render-content obj)
+    (with-html 
+      (:div :class "navigation-body"
+	    (mapc #'render-widget (widget-children obj))))))
 
 (defmethod per-class-dependencies append ((obj navigation))
   (list (make-local-dependency :stylesheet "menu")))
@@ -56,6 +63,7 @@ may be NIL in which case the default pane name is provided."
 	args)
   obj)
 
+;; TODO: rework this, add :header option somewhere
 (defun make-navigation (name &rest args)
   "Instantiates the default navigation widget via 'make-instance'
 and forwards it along with 'args' to 'init-navigation'.
@@ -64,4 +72,23 @@ The navigation widgets bears the title NAME."
   (let ((nav (make-instance 'navigation :name name)))
     (apply #'init-navigation nav args)
     nav))
+
+
+(export '(teleport teleport-source teleport-key))
+
+(defwidget teleport ()
+  ((source :accessor teleport-source
+	   :initarg :source
+	   :documentation "Source widget that should be teleported and
+	   rendered.")
+   (key :accessor teleport-key
+	:initarg :key
+	:initform #'identity
+	:documentation "The function that will be used to access the
+   widget from the source.")))
+
+(defmethod render-widget-body ((obj teleport) &rest args)
+  (declare (ignore args))
+  (render-widget (funcall (teleport-key obj) (teleport-source obj))))
+
 
